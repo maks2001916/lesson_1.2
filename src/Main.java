fcae3f9e173a73621bc89f9bc1efa21042e82737
maks <mamonov.maksim.20003@gmail.com>
2022-08-03 10:14:03 +0300

    завершена третья задача

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,38 @@ public class Main {
         long sign = 222_457_159L;
         float part = 1.5f;
         double particle = 3.847562764653;
-        System.out.println(pencil + sheet + letter + sign + part + particle);
+        char figure = 5;
+        boolean theory = true;
+        System.out.println(pencil + sheet + letter + sign + part + particle + figure);
         //вторая задача
         float weightOfOneBoxer = 78.2f;
         float weightOfTheSecondBoxer = 82.7f;
         float totalWeight = weightOfOneBoxer + weightOfTheSecondBoxer;
         System.out.println("общий вес боксёров " + totalWeight + " кг.");
         float difference = weightOfTheSecondBoxer - weightOfOneBoxer;
-        System.out.println("разница в весе бовсёров " + difference + " кг");
+        System.out.println("разница в весе боксёров " + difference + " кг");
+        //третья задача
+        //количество продуктов
+        byte bananas = 5;
+        short milk = 2;
+        byte iceCream = 2;
+        byte eggs = 4;
+        //масса единицы продукта
+        byte weightOfOneBanana = 80;
+        byte oneHundredMlMilk = 105;
+        byte iceCreamBriquette = 100;
+        byte oneEgg = 70;
+        //подсчёт массы продуктов
+        int weightOfBananas = bananas * weightOfOneBanana;
+        int milkWeight = milk * oneHundredMlMilk;
+        int massOfIceCream = iceCream * iceCreamBriquette;
+        int eggWeight = eggs * oneEgg;
+        long totalMassOfProducts = weightOfBananas + milkWeight + massOfIceCream + eggWeight;
+        long oneKilogramms = 1000;
+        float massInKilogrsms = totalMassOfProducts / oneKilogramms;
+        System.out.println("Масса завтрака " + massInKilogrsms + " кг");
+
+
 
 
     }
